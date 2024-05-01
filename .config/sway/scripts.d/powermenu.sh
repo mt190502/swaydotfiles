4750ee16e1e4f7107cfee6b261d23d1e604147e3
Taha <mtahayagmur190502@gmail.com>
2024-05-01 10:49:48 +0300

    added htop config and some updates

--- .config/sway/scripts.d/powermenu.sh
+++ .config/sway/scripts.d/powermenu.sh
@@ -10,6 +10,7 @@ WAYLAND_DISPLAY=wayland-1
 #~~~ lock function
 blurlock() {
     [[ -n "$(ps aux | grep -v grep | grep swaylock)" ]] && exit
+    touch $HOME/.cache/swaylock.lock
     for output in $(swaymsg -t get_outputs | jq -r '.[].name'); do
         image=/tmp/$output-lock
 	[[ -e $image ]] && rm $image
@@ -17,12 +18,20 @@ blurlock() {
         convert -blur 0x10 $image.png $image-blurred.png
         args="$args --image $output:$image-blurred.png"
     done
-    WAYLAND_DISPLAY=$WAYLAND_DISPLAY swaylock $args --debug &> ~/.cache/swaylock-debug-$(date +%Y-%m-%d_%H-%M-%S).log &
+    WAYLAND_DISPLAY=$WAYLAND_DISPLAY swaylock $args --daemonize
+    rm -f $HOME/.cache/swaylock.lock
 }
 
 #~~~ menu
 [[ "$@" == "--lock" ]] && { blurlock; exit 0; }
 [[ "$@" == "--suspend" ]] && { blurlock; sleep 1; systemctl suspend; exit 0; }
+[[ "$@" == "--rescuelock" ]] && {
+	while [[ -f "$HOME/.cache/swaylock.lock" ]] && [[ -z "$(ps aux | grep -v grep | grep swaylock)" ]]; do
+		blurlock
+		exit 0
+	done
+}
+
 MODE=$(swaynag -t wpgtheme -m PowerMenu -Z Shutdown 'echo 0' -Z Reboot 'echo 1' -Z Suspend 'echo 2' -Z Lock 'echo 3' -Z Logout 'echo 4')
 [[ ! -n "$MODE" ]] && exit
 CONFIRM=$(swaynag -t wpgtheme -m Confirm? -Z No 'echo no' -Z Yes 'echo yes')
@@ -35,12 +44,12 @@ case $MODE in
         systemctl reboot -i
     ;;
     2)
-        blurlock
+        blurlock &
         sleep 1
         systemctl suspend
     ;;
     3)
-        blurlock &
+        blurlock
     ;;
     4)
         swaymsg exit
--- .config/sway/scripts.d/workspace.sh
+++ .config/sway/scripts.d/workspace.sh
@@ -3,6 +3,12 @@
 [[ -z $2 ]] && { notify-send "No workspace specified..."; exit; }
 active_monitor=$(swaymsg -t get_outputs | jq -r '.[] | select (.focused) | .name')
 case $1 in
+	init)
+		for monitor in $(swaymsg -t get_workspaces | jq -r ".[].output"); do
+			swaymsg workspace 1-$monitor output $monitor
+			swaymsg workspace 1-$monitor
+		done
+	;;
 	switch)
 		swaymsg workspace $2-$active_monitor output $active_monitor
 		swaymsg workspace $2-$active_monitor
