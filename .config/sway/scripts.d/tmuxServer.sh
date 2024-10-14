0691323e052b1378e6e47502c6f3d526609b8bf2
Taha <mt190502@mtaha.dev>
2024-10-14 20:36:19 +0300

    Remove unnecessary scripts and configuration files

--- .config/sway/scripts.d/tmuxServer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true; do
-	[[ -z "$(tmux ls | grep daemonmodetmux)"   ]] && { cd $HOME && tmux new-session -ds daemonmodetmux; }
-	[[ -z "$(tmux ls | grep dropterminaltmux)" ]] && { cd $HOME && tmux new-session -ds dropterminaltmux; }
-	sleep 1
-done
